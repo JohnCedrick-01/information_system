041771684c2e2df52bd0df7d265c95f9a6b0247e
JohnCedrick-01 <cedrick@usource.me>
2023-02-10 08:19:12 +0800

    Dashboard Side Panel creation

--- information_system/src/App.js
+++ information_system/src/App.js
@@ -1,23 +1,10 @@
-import logo from '../src/imgs/usource.png';
-import './App.css';
+import './App.css'
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Something great will happen.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.usource.me/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit US!
-        </a>
-      </header>
+       <Sidebar/>
     </div>
   );
 }
